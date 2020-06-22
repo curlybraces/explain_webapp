04a99f01a01fab36325ae5512961301293389ae5
Timothy Antonius <timothyantonius@imac.home>
2020-06-22 20:00:10 +0200

    knobs

--- src/components/ventilator_controls.vue
+++ src/components/ventilator_controls.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <div class="q-gutter-xs q-ma-sm">
-      <label style="width: 200px">tidal vsolume</label>
-      <label>minute volume</label>
-      <label>flow</label>
-      <label>pip</label>
-      <label>peep</label>
-      <label>freq</label>
-      <label>t in</label>
-      <label>fio2</label>
-    </div>
     <div class="q-gutter-sm q-ma-sm">
       <q-knob
         show-value
@@ -20,57 +10,101 @@
         size="100px"
         color="primary"
         track-color="grey-3"
-      />
+      >
+        <div class="text-center">
+          <div>
+            <small class="text-negative text-knob">tidal volume</small>
+          </div>
+          <div>{{ tv }}</div>
+        </div>
+      </q-knob>
+
       <q-knob
         show-value
         class="text-primary q-ma-md"
         v-model="mv"
         :min="min_mv"
         :max="max_mv"
-        size="50px"
+        size="100px"
         color="primary"
         track-color="grey-3"
-      />
+      >
+        <div class="text-center">
+          <div>
+            <small class="text-negative text-knob">minute volume</small>
+          </div>
+          <div>{{ mv }}</div>
+        </div>
+      </q-knob>
+
       <q-knob
         show-value
         class="text-primary q-ma-md"
         v-model="flow"
         :min="min_flow"
         :max="max_flow"
-        size="50px"
+        size="100px"
         color="primary"
         track-color="grey-3"
-      />
+      >
+        <div class="text-center">
+          <div>
+            <small class="text-negative text-knob">inspiratory flow</small>
+          </div>
+          <div>{{ flow }}</div>
+        </div>
+      </q-knob>
       <q-knob
         show-value
         class="text-primary q-ma-md"
         v-model="pip"
         :min="min_pip"
         :max="max_pip"
-        size="50px"
+        size="100px"
         color="primary"
         track-color="grey-3"
-      />
+      >
+        <div class="text-center">
+          <div>
+            <small class="text-negative text-knob">peak pressure</small>
+          </div>
+          <div>{{ pip }}</div>
+        </div>
+      </q-knob>
       <q-knob
         show-value
         class="text-primary q-ma-md"
         v-model="peep"
         :min="min_peep"
         :max="max_peep"
-        size="50px"
+        size="100px"
         color="primary"
         track-color="grey-3"
-      />
+      >
+        <div class="text-center">
+          <div>
+            <small class="text-negative text-knob">peep</small>
+          </div>
+          <div>{{ peep }}</div>
+        </div>
+      </q-knob>
       <q-knob
         show-value
         class="text-primary q-ma-md"
         v-model="freq"
         :min="min_freq"
         :max="max_freq"
-        size="50px"
+        size="100px"
         color="primary"
         track-color="grey-3"
-      />
+      >
+        <div class="text-center">
+          <div>
+            <small class="text-negative text-knob">frequency</small>
+          </div>
+          <div>{{ freq }}</div>
+        </div>
+      </q-knob>
       <q-knob
         show-value
         class="text-primary q-ma-md"
@@ -78,20 +112,34 @@
         :min="min_tin"
         :max="max_tin"
         :step="step_t_in"
-        size="50px"
+        size="100px"
         color="primary"
         track-color="grey-3"
-      />
+      >
+        <div class="text-center">
+          <div>
+            <small class="text-negative text-knob">inspiration time</small>
+          </div>
+          <div>{{ t_in }}</div>
+        </div>
+      </q-knob>
       <q-knob
         show-value
         class="text-primary q-ma-md"
         v-model="fio2"
         :min="min_fio2"
         :max="max_fio2"
-        size="50px"
+        size="100px"
         color="primary"
         track-color="grey-3"
-      />
+      >
+        <div class="text-center">
+          <div>
+            <small class="text-negative text-knob">fio2</small>
+          </div>
+          <div>{{ fio2 }}</div>
+        </div>
+      </q-knob>
     </div>
     <q-btn @click="updateVentilatorSettings"> update </q-btn>
   </div>
@@ -149,4 +197,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.text-knob {
+  font-size: 10px;
+}
+</style>
